c172c9015d12ff79399c564fd3d6ccc9285f94c6
aldojunior990 <aldojunior990@gmail.com>
2024-08-15 14:06:24 -0300

    first commit

---
+++ src/main/java/com/pratopronto/prato_pronto_api/PratoProntoApiApplication.java
@@ -0,0 +1,13 @@
+package com.pratopronto.prato_pronto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratoProntoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PratoProntoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratopronto/prato_pronto_api/PratoProntoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratopronto.prato_pronto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratoProntoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
